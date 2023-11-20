e1df5f4647666327814a26e2ee33ea98d956862a
hknocal <hknocal@outlook.com>
2023-11-20 10:50:42 +0100

    initialize repo

---
+++ src/main/java/com/kea/cosmeticsbackend/CosmeticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kea.cosmeticsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmeticsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kea/cosmeticsbackend/CosmeticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.cosmeticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
