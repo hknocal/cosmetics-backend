ace3c1e5701c836a03d38e8e56b6708b902e5cf3
hknocal <hknocal@outlook.com>
2023-12-05 00:08:44 +0100

    cors fix

--- src/main/java/com/kea/cosmeticsbackend/controller/TreatmentController.java
+++ src/main/java/com/kea/cosmeticsbackend/controller/TreatmentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/treatment")
+@CrossOrigin(origins = "https://happy-sky-0a8b63303.4.azurestaticapps.net")
 public class TreatmentController {
 
     @Autowired
